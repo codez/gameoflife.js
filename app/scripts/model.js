a7b2d1dc53df537597b2299d6b64d57346185cd8
Pascal Zumkehr <spam@codez.ch>
2014-10-04 18:28:05 +0200

    scope variables and functions to get private behavior

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -5,111 +5,115 @@
     'use strict';
 
 	function GameOfLife() {
-     	this.stepInterval = 100;
-		this.cells = [];
-	};
+     	var stepInterval = 100;
+		var cells = [];
+        var countX = 0, countY = 0;
+        var runner = null;
 
-    GameOfLife.prototype.init = function(countX, countY) {
-    	this.countX = countX;
-    	this.countY = countY;
+        this.init = function(_countX, _countY) {
+        	countX = _countX;
+        	countY = _countY;
 
-    	this.iterateCells(
-    		function(x,y) { this.cells[y][x] = false; },
-    		function(y) { this.cells[y] = []; });
-    };
+        	this.iterateCells(
+        		function(x,y) { cells[y][x] = false; },
+        		function(y) { cells[y] = []; });
+        };
 
-    GameOfLife.prototype.iterateCells = function(cellCallback, rowCallback) {
-    	for (var y = 0; y < this.countY; y++) {
-    		if (rowCallback) { rowCallback.apply(this, [y]); }
-    		for (var x = 0; x < this.countX; x++) {
-    			cellCallback.apply(this, [x, y]);
+        this.iterateCells = function(cellCallback, rowCallback) {
+        	for (var y = 0; y < countY; y++) {
+        		if (rowCallback) { rowCallback.apply(this, [y]); }
+        		for (var x = 0; x < countX; x++) {
+        			cellCallback.apply(this, [x, y]);
+        		}
+        	}
+        };
+
+        this.toggleCell = function(x, y, state) {
+        	if (state === undefined) {
+        		cells[y][x] = !cells[y][x];
+    	    } else {
+    	    	cells[y][x] = state;
+    	    }
+        	//$cell.trigger('cell.update', [cells[y][x]]);
+        	return cells[y][x];
+        };
+
+    	this.start = function() {
+    		var _this = this;
+    		runner = setInterval(
+    			function() { _this.step(); },
+    			stepInterval);
+
+    	};
+
+    	this.stop = function() {
+    		if (runner) {
+    			clearInterval(runner);
+    			runner = null;
     		}
-    	}
-    };
+    	};
 
-    GameOfLife.prototype.toggleCell = function(x, y, state) {
-    	if (state === undefined) {
-    		this.cells[y][x] = !this.cells[y][x];
-	    } else {
-	    	this.cells[y][x] = state;
-	    }
-    	//$cell.trigger('cell.update', [this.cells[y][x]]);
-    	return this.cells[y][x];
-    };
+    	this.isRunning = function() {
+    		return runner !== null;
+    	};
 
-	GameOfLife.prototype.start = function() {
-		var _this = this;
-		this.runner = setInterval(
-			function() { _this.step(); },
-			this.stepInterval);
+    	this.step = function() {
+    		var result = [];
+    		this.iterateCells(
+    			function(x, y) { computeResultCell(result, x, y); },
+    			function(y) { result[y] = []; });
 
-	};
+    	    cells = result;
+    	};
 
-	GameOfLife.prototype.stop = function() {
-		if (this.runner) {
-			clearInterval(this.runner);
-			this.runner = null;
-		}
-	};
+    	var computeResultCell = function(result, x, y) {
+    		var count = countNeighbours(x, y);
 
-	GameOfLife.prototype.isRunning = function() {
-		return this.runner != null;
-	}
+            if (count === 3) {
+            	result[y][x] = true;
+            	if (!cells[y][x]) {
+            		$('#' + x + '_' + y).trigger('cell.update', [true]);
+            	}
+            } else if (count === 4) {
+            	result[y][x] = cells[y][x];
+            } else {
+            	result[y][x] = false;
+            	if (cells[y][x]) {
+            		$('#' + x + '_' + y).trigger('cell.update', [false]);
+            	}
+            }
+    	};
 
-	GameOfLife.prototype.step = function() {
-		var result = [];
-		this.iterateCells(
-			function(x, y) { this.computeResultCell(result, x, y); },
-			function(y) { result[y] = []; });
+    	var countNeighbours = function(x, y) {
+    		var amount = 0;
 
-	    this.cells = result;
-	};
+            if (isActive(x-1, y-1)) { amount++; }
+            if (isActive(x,   y-1)) { amount++; }
+            if (isActive(x+1, y-1)) { amount++; }
+            if (isActive(x-1, y  )) { amount++; }
+            if (isActive(x  , y  )) { amount++; }
+            if (isActive(x+1, y  )) { amount++; }
+            if (isActive(x-1, y+1)) { amount++; }
+            if (isActive(x,   y+1)) { amount++; }
+            if (isActive(x+1, y+1)) { amount++; }
 
-	GameOfLife.prototype.computeResultCell = function(result, x, y) {
-		var count = this.countNeighbours(x, y);
+            return amount;
+    	};
 
-        if (count === 3) {
-        	result[y][x] = true;
-        	if (!this.cells[y][x]) {
-        		$('#' + x + '_' + y).trigger('cell.update', [true])
-        	}
-        } else if (count === 4) {
-        	result[y][x] = this.cells[y][x]
-        } else {
-        	result[y][x] = false;
-        	if (this.cells[y][x]) {
-        		$('#' + x + '_' + y).trigger('cell.update', [false])
-        	}
-        }
-	}
+    	var isActive = function(x, y) {
+    		if (y < 0)       { y = countY - 1; }
+    	    if (y >= countY) { y = 0; }
+    		if (x < 0)       { x = countX - 1; }
+    	    if (x >= countX) { x = 0; }
 
-	GameOfLife.prototype.countNeighbours = function(x, y) {
-		var amount = 0;
+            return cells[y][x];
+        };
 
-        if (this.isActive(x-1, y-1)) amount++;
-        if (this.isActive(x,   y-1)) amount++;
-        if (this.isActive(x+1, y-1)) amount++;
-        if (this.isActive(x-1, y  )) amount++;
-        if (this.isActive(x  , y  )) amount++;
-        if (this.isActive(x+1, y  )) amount++;
-        if (this.isActive(x-1, y+1)) amount++;
-        if (this.isActive(x,   y+1)) amount++;
-        if (this.isActive(x+1, y+1)) amount++;
-
-        return amount;
-	};
-
-	GameOfLife.prototype.isActive = function(x, y) {
-		if (y < 0) y = this.countY - 1;
-	    if (y >= this.countY) y = 0;
-		if (x < 0) x = this.countX - 1;
-	    if (x >= this.countX) x = 0;
-
-        return this.cells[y][x];
-    };
+    }
 
     window.GameOfLife = {
     	Model: GameOfLife
     };
+
 
 })(); // immediate function
--- app/scripts/view.js
+++ app/scripts/view.js
@@ -1,97 +1,110 @@
-$(function() {
+(function() {
 	'use strict';
 
 	function View() {
-		this.gridSize = 10;
-		this.model = new window.GameOfLife.Model();
+		var gridSize = 10;
+		var model = new window.GameOfLife.Model();
 
-		this.init();
-	}
+		this.init = function() {
+			measureWorld();
+			initWorld();
+			initEventHandlers();
+		};
 
-	View.prototype.init = function() {
-		this.measureWorld();
-		this.initWorld();
-		this.initEventHandlers();
-	}
+		var measureWorld = function() {
+			var totalHeight = $(window).innerHeight() - $('#header').outerHeight();
+			var totalWidth = $('#world').innerWidth();
+			var countX = Math.round(totalWidth / gridSize);
+			var countY = Math.round(totalHeight / gridSize);
 
-	View.prototype.measureWorld = function() {
-		var totalHeight = $(window).innerHeight() - $('#header').outerHeight();
-		var totalWidth = $('#world').innerWidth();
-		var countX = Math.round(totalWidth / this.gridSize);
-		var countY = Math.round(totalHeight / this.gridSize);
+			console.log('world is ' + totalWidth + 'x' + totalHeight);
+			console.log('create ' + countX + 'x' + countY + ' cells');
 
-		console.log('world is ' + totalWidth + 'x' + totalHeight)
-		console.log('create ' + countX + 'x' + countY + ' cells');
+		    $('#world').css({ height: totalHeight });
+		    model.init(countX, countY);
+		};
 
-        $('#world').css({ height: totalHeight });
-        this.model.init(countX, countY);
-	};
+		var initWorld = function() {
+			var row;
+			model.iterateCells(
+				function(x, y) {
+					row.append('<td id="' + x + '_' + y +
+						       '" data-x="' + x +
+						       '" data-y="' + y + '"></td>');
+				},
+				function() {
+					$('#world table tbody').append('<tr></tr>');
+					row = $('#world table tbody tr:last-child');
+				});
+		};
 
-    View.prototype.initWorld = function() {
-    	var row;
-    	this.model.iterateCells(
-    		function(x, y) {
-    			row.append('<td id="' + x + '_' + y +
-					       '" data-x="' + x +
-					       '" data-y="' + y + '"></td>');
-    		},
-    		function() {
-    			$('#world table tbody').append('<tr></tr>');
-    			row = $('#world table tbody tr:last-child');
-    		});
-	};
+		var toggleCell = function(cell, state) {
+			var $cell = $(cell);
+			var x = $cell.data('x');
+			var y = $cell.data('y');
+			var newState = model.toggleCell(x, y, state);
+			updateCell(cell, newState);
+			return newState;
+		};
 
-	View.prototype.toggleCell = function(cell, state) {
-		var $cell = $(cell);
-    	var x = $cell.data('x');
-    	var y = $cell.data('y');
-		var newState = this.model.toggleCell(x, y, state);
-		this.updateCell(cell, newState);
-		return newState;
-	}
+		var updateCell = function(cell, state) {
+			if (state) { $(cell).addClass('on'); }
+		    else { $(cell).removeClass('on'); }
+		};
 
-	View.prototype.updateCell = function(cell, state) {
-		if (state) { $(cell).addClass('on'); }
-	    else { $(cell).removeClass('on'); }
-	}
-
-    View.prototype.initEventHandlers = function() {
-    	var _this = this;
-    	$(window).resize(function() {   });
-
-	    (function() {
-	    	var activate = undefined;
-	    	$(document).on('mousedown', '#world td', function(event) {
-	    		activate = _this.toggleCell(this);
-	    	}).on('mouseup', function(event) {
+		var registerCellDrawEvents = function() {
+	    	var activate;
+	    	$(document).on('mousedown', '#world td', function() {
+	    		activate = toggleCell(this);
+	    	}).on('mouseup', function() {
 	    		activate = undefined;
-	    	}).on('mouseenter', '#world td', function(event) {
-	    		if (activate != undefined) {
-	    		  _this.toggleCell(this, activate);
+	    	}).on('mouseenter', '#world td', function() {
+	    		if (activate !== undefined) {
+	    		  toggleCell(this, activate);
 	    		}
 	    	});
-	    })();
+	    };
 
-	    $(document).on('cell.update', '#world td', function(event, state) {
-	    	_this.updateCell(this, state);
-	    });
+	    var registerCellUpdateEvent = function() {
+	    	$(document).on('cell.update', '#world td', function(event, state) {
+		    	updateCell(this, state);
+		    });
+	    };
 
-	    $(document).on('click', '#stepper', function(event) {
-	    	_this.model.step();
-	    })
+	    var registerStepEvent = function() {
+	    	$(document).on('click', '#stepper', function() {
+		    	model.step();
+		    });
+	    };
 
-	    $(document).on('click', '#starter', function(event) {
-	    	if (_this.model.isRunning()) {
-	    	   _this.model.stop();
-	    	   $(this).html('Start');
-	        }
-	    	else {
-	    		_this.model.start();
-	    		$(this).html('Stop');
-	    	}
-	    })
-    };
+	    var registerStartStopEvent = function() {
+	    	$(document).on('click', '#starter', function() {
+		    	if (model.isRunning()) {
+		    	   model.stop();
+		    	   $(this).html('Start');
+		        }
+		    	else {
+		    		model.start();
+		    		$(this).html('Stop');
+		    	}
+		    });
+	    };
+
+	    var registerResizeEvent = function() {
+	    	$(window).resize(function() {   });
+	    };
+
+		var initEventHandlers = function() {
+		    registerCellDrawEvents();
+		    registerCellUpdateEvent();
+		    registerStartStopEvent();
+		    registerStepEvent();
+		    registerResizeEvent();
+		};
+
+		this.init();
+    }
 
     window.GameOfLife.View = View;
 
-});
+})();
